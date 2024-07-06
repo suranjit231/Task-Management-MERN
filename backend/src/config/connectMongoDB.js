b5a7e7d8f4a11984b352b2dd30cd6b1bbab31395
suranjit78567cls <namasudrasuranjit164@gmail.com>
2024-07-06 08:43:08 -0700

    first commit

--- backend/src/config/connectMongoDB.js
+++ backend/src/config/connectMongoDB.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 //========= connecting mongodb using mongoose =================//
 const connectMongodb = async()=>{
     const url = `${process.env.DB_URl}/Task_DB`
+    // const url = 'mongodb+srv://namasudrasuranjit164:rahul2314@cluster0.cpyvocf.mongodb.net/Task_DB?retryWrites=true&w=majority&appName=Cluster0';
   
     try{
         await mongoose.connect(url,
@@ -16,6 +17,7 @@ const connectMongodb = async()=>{
         console.log("mongodb is connected!")
 
     }catch(error){
+        console.log(error);
         console.log("mongodb connection falid!")
 
     }
